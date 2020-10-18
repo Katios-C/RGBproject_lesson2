2180ed664b3812e2c625d5e3c193c197d5d99ac7
Katios <kate.chonkina@gmail.com>
2020-10-18 15:56:29 +0600

    Initial Commit

---
+++ RGBproject_lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBproject_lesson2
+//
+//  Created by Екатерина Чернова on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
